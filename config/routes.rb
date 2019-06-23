ae2e80447f4cc04785834cdb30f6d63674a2d690
kaitomdion <zz5tprefyhgx299xjsyx@docomo.ne.jp>
2019-06-23 16:28:03 +0900

    コントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root to: "items#index"
-  resources :users
-  resources :items
-   collection do
-    get 'confirm'
-
-  resources :cards
+  root "items#index"
+  resources :users, only: [:new, :show, :edit]
+  resources :items, only: [:index, :show, :new]
+  resources :cards, only: [:new]
 end
