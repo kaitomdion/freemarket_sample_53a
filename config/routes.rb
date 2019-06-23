6ebd1da77e7f55dafe6abc12887e6cc28ef6aa37
Norimitsu-Shimizu <sparesound31@gmail.com>
2019-06-23 18:21:22 +0900

    items collectionにdoを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "items#index"
   resources :users, only: [:new, :show, :edit]
-  resources :items, only: [:index, :show, :new]
+  resources :items, only: [:index, :show, :new] do
     collection do
       get 'confirm'
     end
+  end
   resources :cards, only: [:new]
 
 end
