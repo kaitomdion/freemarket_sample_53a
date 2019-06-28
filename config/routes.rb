d2c9c06aebcd80e7a553fedd8d2fc15310dfaaf7
kenkenpi0526 <kenkenpi0526@gmail.com>
2019-06-28 14:17:38 +0900

    (wip)add user mypage(show/edit)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "items#index"
-  resources :users, only: [:new, :show, :edit]
+  resources :users, only: [:new, :show, :edit ,:index]
   resources :items, only: [:index, :show, :new] do
     collection do
       get 'confirm'
