cadfaeff30fb3460e5d7e5abc3861f1f015675a9
kaitomdion <zz5tprefyhgx299xjsyx@docomo.ne.jp>
2019-06-29 17:54:30 +0900

    イメージテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_084829) do
+ActiveRecord::Schema.define(version: 2019_06_29_085143) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2019_06_29_084829) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "url", null: false
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -117,6 +127,8 @@ ActiveRecord::Schema.define(version: 2019_06_29_084829) do
   end
 
   add_foreign_key "cards", "users"
+  add_foreign_key "images", "items"
+  add_foreign_key "images", "users"
   add_foreign_key "items", "shipping_burdens"
   add_foreign_key "items", "shipping_days"
   add_foreign_key "items", "shipping_methods"
