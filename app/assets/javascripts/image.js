3b558f780114dbd2ef7b1ebb220f967b1241b351
kaitomdion <zz5tprefyhgx299xjsyx@docomo.ne.jp>
2019-07-03 19:04:19 +0900

    商品出品　途中２

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -5,17 +5,11 @@ $(function(){
         reader = new FileReader();
         $preview = $("#file-preview");
         t = this;
-        // img = $(`<div class= "add_img"><div class="img_area"><img class="image"></div></div>`);
-        // btn_wrapper = $('<div class="btn_wrapper"><a class="btn_edit">編集</a><a class="btn_delete">削除</a></div>');
-
-        // $preview.append(btn_wrapper);
     reader.onload = (function(file) {
       return function(e) {
-        // $preview.empty();
         $preview.append($('<img><t">編集</a><a class="btn_delete">削除</a></div>').attr({
           src: e.target.result,
-          width: "150px",
-          // height: "100px",
+          width: "150px",       
           class: "preview",
           title: file.name
         }));
