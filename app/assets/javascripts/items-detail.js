9de3cb24cdebe56b40f6d7970a563bde9f3339e8
kenkenpi0526 <kenkenpi0526@gmail.com>
2019-07-10 20:26:59 +0900

    commit modify

--- app/assets/javascripts/items-detail.js
+++ app/assets/javascripts/items-detail.js
@@ -1,6 +1,6 @@
-$(function(){
-  $('img.is-higher-width').mouseover(function(){
-   var $thisImg = $(this).attr('src');
-   $('img.owl-lazy').attr({src:$thisImg});
-  });
- });
+// $(function(){
+//   $('img.is-higher-width').mouseover(function(){
+//    var $thisImg = $(this).attr('src');
+//    $('img.owl-lazy').attr({src:$thisImg});
+//   });
+//  });
