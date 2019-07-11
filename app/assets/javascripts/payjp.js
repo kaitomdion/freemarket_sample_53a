5d6f008205f0e31ae1d1a92ccd428d9606ce403b
kameno130 <51069500+kameno130@users.noreply.github.com>
2019-07-11 14:50:08 +0900

    コントローラの編集。クレジットカード情報登録完了

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,45 +1,7 @@
-// $(document).on('turbolinks:load', function() {
-//   var form = $("#card_form");
-//   Payjp.setPublicKey("pk_test_731ea1b57bd2b5a7ec83783a");
-// //まずはテスト鍵をセットする↑
-//   $("#credit_form_btn").on("click", "#token_submit",function(e){
-//     e.preventDefault();
-//     console.log('aaaaa')
-//   //↑ここでrailsの処理を止めることでjsの処理を行う
-//     var card = {
-//       number: $("#card_number").val(),
-//       cvc: $("#cvc").val(),
-//       exp_month: $("#exp_month").val(),
-//       exp_year: $("#exp_year").val()
-//     };
-//    //↑Pay.jpに登録するデータを準備する
-//     Payjp.createToken(card,function(status,response){
-//    //↑先ほどのcard情報がトークンという暗号化したものとして返ってくる
-//       form.find("input[type=submit]").prop("disabled", true);
-//       if(status == 200){//←うまくいった場合200になるので
-//         $("#card_number").removeAttr("name");
-//         $("#cvc").removeAttr("name");
-//         $("#exp_month").removeAttr("name");
-//         $("#exp_year").removeAttr("name");
-//        //↑このremoveAttr("name")はデータを保持しないように消している
-//         var payjphtml = `<input type="hidden" name="payjpToken" value=${response.id}>`
-//         form.append(payjphtml);
-//         //↑これはdbにトークンを保存するのでjsで作ったトークンをセットしてる
-//         document.inputForm.submit();
-//        //↑そしてここでsubmit！！これでrailsのアクションにいく！もちろん上でトークンをセットしているのでparamsの中には{payjpToken="トークン"}という情報が入っている
-//       }else{
-//         alert("カード情報が正しくありません。");
-//       }
-//     });
-//   });
-// });
-
-
 $(document).on('turbolinks:load', function() {
   var form = $("#charge-form");
   Payjp.setPublicKey("pk_test_731ea1b57bd2b5a7ec83783a");
   $(document).on("click", "#submit-button", function(e) {
-
     e.preventDefault();
     form.find("input[type=submit]").prop("disabled", true);
 
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,27 +10,26 @@ class CardsController < ApplicationController
     redirect_to action: "show" if card.exists?
   end
 
-  def create #クレジットカード登
+  def create 
     Payjp.api_key =  Rails.application.credentials.pay_jp[:secret_access_key]
-    # ここでテスト鍵をセットしてあげる(環境変数にしても良い)
     if params['payjpToken'].blank?
-    # paramsの中にjsで作った'payjpTokenが存在するか確かめる
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
       card: params['payjpToken'],
       metadata: {user_id: current_user.id}
       )
-     # ↑ここでpay.jpに保存
-      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-     # ここでdbに保存
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+
       if @card.save
         redirect_to controller: "items", action: 'index'
         flash[:notice] = 'クレジットカードの登録が完了しました'
       else
-        redirect_to controller: "cards", action: 'new'
+        redirect_to  action: 'new'
         flash[:alert] = 'クレジットカード登録に失敗しました'
+        
       end
     end
  end
 end
+
